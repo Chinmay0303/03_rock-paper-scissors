d395ba6e4ec4786e9de95f0b9c20b73f3d4020f6
Chinmay0303 <99880235+Chinmay0303@users.noreply.github.com>
2025-02-05 11:39:39 +0530

    write a plan or pseudocode

--- javascript.js
+++ javascript.js
@@ -1,3 +1,13 @@
 const log = console.log;
 
 log("Hello, World;");
+
+// ask user for input (rock, paper, scissors)
+// generate random computer choice
+// compare the two
+// decide a winner
+// if computer wins -> computer score increases by one
+// if user wins -> user score increases by one
+// repeat this 5 times
+// after 5 rounds compare both the score
+// declare the ultimate winner
