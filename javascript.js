6b55c27a3a1c5d7dbd4715a1c475724a7a12267f
Chinmay0303 <99880235+Chinmay0303@users.noreply.github.com>
2025-02-05 12:08:54 +0530

    create a while loop for taking right user input

--- javascript.js
+++ javascript.js
@@ -5,10 +5,26 @@ log("Hello, World;");
 // ask user for input (rock, paper, scissors)
 
 let userChoice;
-userChoice=prompt("Choose one: 1-Rock, 2-Paper or 3-Scissors");
 
-log(userChoice);
-log(typeof userChoice);
+// I don't want the user to enter anything other than the given choices: 1, 2 or 3.
+
+// create a while loop for ascertaining this
+
+let choice = false;
+
+while(choice === false){
+    
+    userChoice = prompt("Choose one: 1-Rock, 2-Paper or 3-Scissors");
+
+    log("Enter either 1,2 or 3");
+
+    if(userChoice == 1 || userChoice == 2 || userChoice == 3){
+        choice = true;
+    }
+}
+
+log({userChoice});
+// log(typeof userChoice);
 
 // generate random computer choice
 // compare the two
