98831c4043ce3fcb9e946f71856b2a093816b22e
Chinmay0303 <99880235+Chinmay0303@users.noreply.github.com>
2025-02-11 15:22:11 +0530

    add click event listeners
    add functionality to play and reset buttons

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
 const log = console.log;
 
 log('main');
+
+const playButton = document.querySelector('.play.button');
+const resetButton = document.querySelector('.reset.button');
+
+// making use of event bubbling
+// adding eventListener on the whole document rather than on individual buttons
+document.addEventListener('click', clickFunction);
+
+function clickFunction(event){
+    let buttonClicked = event.target.classList.value;
+    // gives class of the clicked button
+
+    if(buttonClicked === 'reset button'){
+        resetButtonClicked();
+    }
+    else if(buttonClicked === 'play button'){
+        playButtonClicked();
+    }
+}
+
+function resetButtonClicked(){
+    log('RESET');
+    resetButton.disabled = true;
+    playButton.disabled = false;
+}
+
+function playButtonClicked(){
+    log('PLAY');
+    playButton.disabled = true;
+    resetButton.disabled = false;
+}
